06c15bfd755b5de7ea9b2b93da2e5395aa3fb77e
scarmo <scarmody@tcd.ie>
2018-10-12 11:32:36 +0100

    fixed error scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,11 @@
 
 # bash script to execute while on AWS instance
 
+# run:
+# sudo /bin/bash NVIDIA-Linux-x86_64-396.44.run
+#to setup gpu
+
+
 #hashcat set-up
 cd ~/
 sudo apt update
@@ -16,3 +21,11 @@ git submodule update --init
 sudo make
 sudo checkinstall
 hashcat --version
+cd ~/
+
+# Now to get rockyou.txt & drivers
+wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
+
+# git clone my repo to get access to files
+
+git clone https://github.com/SCarmo/Scalable-Computing.git
--- scripts/sha256.sh
+++ scripts/sha256.sh
@@ -2,7 +2,7 @@
 hashcat -m 7400 -a 0 -w 4 -o sha256cracked.txt ~/Scalable-Computing/hashes/sha256.txt rockyou.txt --rules=rules/best64.rule
 
 # sha256 combination attack 
-hashcat -m 7400 -a 1 -w 4 -o sha256cracked.txt ~/Scalable-Computing/hashes/sha256.txt ~/Scalable-Computing/word2.txt ~/Scalable-Computing/word2.txt 
+hashcat -m 7400 -a 1 -w 4 -o sha256cracked.txt ~/Scalable-Computing/hashes/sha256.txt ~/Scalable-Computing/words2.txt ~/Scalable-Computing/words2.txt 
 
 # 5 lower case letters
 hashcat -m 7400 -a 3 -w 4 -o sha256cracked.txt ~/Scalable-Computing/hashes/sha256.txt ?l?l?l?l?l
