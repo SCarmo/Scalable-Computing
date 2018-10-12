709a238776546ac3bfa233a1c1d12ba8a48b2160
scarmo <scarmody@tcd.ie>
2018-10-12 14:16:02 +0100

    new wordlists and scripts

--- scripts/sha256.sh
+++ scripts/sha256.sh
@@ -1,8 +1,8 @@
 # sha256 rockyou
-hashcat -m 7400 -a 0 -w 4 -o sha256cracked.txt ~/Scalable-Computing/hashes/sha256.txt rockyou.txt --rules=rules/best64.rule
+hashcat -m 7400 -a 0 -w 4 -O -o sha256cracked.txt ~/Scalable-Computing/hashes/sha256.txt rockyou.txt --rules=rules/best64.rule
 
 # sha256 combination attack 
-hashcat -m 7400 -a 1 -w 4 -o sha256cracked.txt ~/Scalable-Computing/hashes/sha256.txt ~/Scalable-Computing/words2.txt ~/Scalable-Computing/words2.txt 
+hashcat -m 7400 -a 1 -w 4 -O -o sha256cracked.txt ~/Scalable-Computing/hashes/sha256.txt ~/Scalable-Computing/words2.txt 
 
 # 5 lower case letters
-hashcat -m 7400 -a 3 -w 4 -o sha256cracked.txt ~/Scalable-Computing/hashes/sha256.txt ?l?l?l?l?l
+hashcat -m 7400 -a 3 -w 4 -O -o sha256cracked.txt ~/Scalable-Computing/hashes/sha256.txt ?l?l?l?l?l
