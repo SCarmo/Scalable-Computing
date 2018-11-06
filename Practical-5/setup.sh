fb692e12c2bb2595b16dbf7f15ca2ad22ad4f669
scarmo <scarmody@tcd.ie>
2018-11-06 16:43:35 +0000

    New setup script

--- Practical-5/setup.sh
+++ Practical-5/setup.sh
@@ -2,15 +2,6 @@ sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt update
 sudo apt install gcc build-essential git automake yasm libgmp-dev libpcap-dev pkg-config libssl-dev libevent-dev libncurses5-dev libbz2-dev zlib1g-dev nvidia-opencl-dev libopenmpi-dev openmpi-bin -y
 
-# get rockyou
-wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
-# hashcat setup
-git clone https://github.com/hashcat/hashcat.git
-cd ./hashcat
-git submodule update --init
-make 
-sudo make install
-cd ..
 
 # tmux setup
 rm -fr /tmp/tmux
@@ -22,13 +13,18 @@ sudo make install
 cd ~/
 rm -fr /tmp/tmux
 
+
 # get driver
 wget http://us.download.nvidia.com/tesla/396.44/NVIDIA-Linux-x86_64-396.44.run
-
 # install driver
 sudo /bin/bash NVIDIA-Linux-x86_64-396.44.run
 
-# get john the ripper
+# get crackstation
+echo "This may take a while..."
+wget https://crackstation.net/files/crackstation.txt.gz
+
+# unzip
+gunzip crackstation.txt.gz crackstation.txt
 mkdir -p ~/JohnTheRipper
 cd ~/JohnTheRipper
 git clone git://github.com/magnumripper/JohnTheRipper -b bleeding-jumbo john
@@ -36,6 +32,4 @@ cd ~/JohnTheRipper/john/src
 ./configure --enable-mpi && make -s clean && make -sj4
 ../run/john --test
 
-
-# perform reboot for driver
 sudo reboot
