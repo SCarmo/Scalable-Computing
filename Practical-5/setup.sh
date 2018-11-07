acdb1d3ed88af328326c660b0cea18f21e79d895
scarmo <scarmody@tcd.ie>
2018-11-07 14:10:09 +0000

    things in wrong order, tmux unnessecary

--- Practical-5/setup.sh
+++ Practical-5/setup.sh
@@ -3,29 +3,20 @@ sudo apt update
 sudo apt install gcc build-essential git automake yasm libgmp-dev libpcap-dev pkg-config libssl-dev libevent-dev libncurses5-dev libbz2-dev zlib1g-dev nvidia-opencl-dev libopenmpi-dev openmpi-bin -y
 
 
-# tmux setup
-rm -fr /tmp/tmux
-git clone https://github.com/tmux/tmux.git /tmp/tmux
-cd /tmp/tmux
-sh autogen.sh
-./configure && make
-sudo make install
-cd ~/
-rm -fr /tmp/tmux
-
-
 # get driver
 wget http://us.download.nvidia.com/tesla/396.44/NVIDIA-Linux-x86_64-396.44.run
 # install driver
 sudo /bin/bash NVIDIA-Linux-x86_64-396.44.run
 
 
-# unzip
-gunzip crackstation.txt.gz crackstation.txt
 
 # get crackstation
 echo "This may take a while..."
 wget https://crackstation.net/files/crackstation.txt.gz
+
+
+# unzip
+gunzip crackstation.txt.gz crackstation.txt
 
 # filter crackstation
 egrep -a "^[[:alnum:]]{5,8}$" crackstation.txt > layer9.txt
