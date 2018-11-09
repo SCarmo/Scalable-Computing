cd43c74f0e9c9dd42b69fe94714b2539c6ee92c5
scarmo <scarmody@tcd.ie>
2018-11-09 12:00:43 +0000

    Perfect script now

--- Practical-5/setup.sh
+++ Practical-5/setup.sh
@@ -15,12 +15,30 @@ echo "This may take a while..."
 wget https://crackstation.net/files/crackstation.txt.gz
 
 
-# unzip
+# unzip, this is 15Gb so make sure theres enough space on disk
 gunzip crackstation.txt.gz crackstation.txt
 
 # filter crackstation
 egrep -a "^[[:alnum:]]{5,8}$" crackstation.txt > layer9.txt
 
+
+fspec=layer9.txt
+num_files=4
+
+# Work out lines per file.
+
+total_lines=$(wc -l <${fspec})
+((lines_per_file = (total_lines + num_files - 1) / num_files))
+
+# Split the actual file, maintaining lines.
+
+split --lines=${lines_per_file} ${fspec} xyzzy.
+
+# Debug information
+
+echo "Total lines     = ${total_lines}"
+echo "Lines  per file = ${lines_per_file}"    
+wc -l xyzzy.*
 
 
 mkdir -p ~/JohnTheRipper
